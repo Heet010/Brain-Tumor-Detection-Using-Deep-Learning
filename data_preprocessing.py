a2e9b6d93d69b0e9318c2ca8c7e3b14f619f48d6
heet010 <heetbhalani99@gmail.com>
2025-04-14 22:58:37 +0200

    added correect dataset folder path

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -320,7 +320,7 @@ def main():
     preprocessor = DataPreprocessor(image_size=(256, 256))
     
     # Load dataset (adjust path as needed)
-    data_path = "path/to/lgg-mri-segmentation"
+    data_path = r"/home/heet_bhalani/Downloads/archive/lgg-mri-segmentation/kaggle_3m"
     
     try:
         image_paths, mask_paths, labels = preprocessor.load_kaggle_lgg_dataset(data_path)
