e5bfc1741b7829c4c7e9c5ec54ab7b41826d7ccf
heet010 <heetbhalani99@gmail.com>
2025-04-15 23:03:23 +0200

    created file for models and added basic residual block for Resnet

---
+++ models.py
@@ -0,0 +1,44 @@
+"""
+Neural network models for brain tumor detection
+Includes ResNet for classification and ResUNet for segmentation
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+from typing import Optional
+import math
+
+
+class ResidualBlock(nn.Module):
+    """Basic residual block for ResNet"""
+    
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, 
+                              stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+            
+        out += identity
+        out = self.relu(out)
+        
+        return out
