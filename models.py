e1766af788b3091aa824179dd09410be24e94d0a
heet010 <heetbhalani99@gmail.com>
2025-04-18 23:24:10 +0200

    added unet model for comperision

--- models.py
+++ models.py
@@ -243,3 +243,42 @@ class ResUNet(nn.Module):
         logits = self.outc(x)
         
         return logits
+
+class UNet(nn.Module):
+    """Standard U-Net model for comparison"""
+    
+    def __init__(self, n_channels: int = 3, n_classes: int = 2, bilinear: bool = True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        
+        # Encoder
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128, use_residual=False)
+        self.down2 = Down(128, 256, use_residual=False)
+        self.down3 = Down(256, 512, use_residual=False)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor, use_residual=False)
+        
+        # Decoder
+        self.up1 = Up(1024, 512 // factor, bilinear, use_residual=False)
+        self.up2 = Up(512, 256 // factor, bilinear, use_residual=False)
+        self.up3 = Up(256, 128 // factor, bilinear, use_residual=False)
+        self.up4 = Up(128, 64, bilinear, use_residual=False)
+        self.outc = OutConv(64, n_classes)
+        
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        
+        return logits
