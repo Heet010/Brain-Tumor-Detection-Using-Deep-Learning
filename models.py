b073b2ce96f150dbf928cf57ced19ce0cfd08395
heet010 <heetbhalani99@gmail.com>
2025-04-18 23:19:48 +0200

    added resnet model for segmentation

--- models.py
+++ models.py
@@ -187,3 +187,59 @@ class OutConv(nn.Module):
         
     def forward(self, x):
         return self.conv(x)
+
+class ResUNet(nn.Module):
+    """ResUNet model for brain tumor segmentation"""
+    
+    def __init__(self, n_channels: int = 3, n_classes: int = 2, bilinear: bool = True):
+        super(ResUNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        
+        # Encoder
+        self.inc = ResidualDoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128, use_residual=True)
+        self.down2 = Down(128, 256, use_residual=True)
+        self.down3 = Down(256, 512, use_residual=True)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor, use_residual=True)
+        
+        # Decoder
+        self.up1 = Up(1024, 512 // factor, bilinear, use_residual=True)
+        self.up2 = Up(512, 256 // factor, bilinear, use_residual=True)
+        self.up3 = Up(256, 128 // factor, bilinear, use_residual=True)
+        self.up4 = Up(128, 64, bilinear, use_residual=True)
+        self.outc = OutConv(64, n_classes)
+        
+        # Initialize weights
+        self._initialize_weights()
+        
+    def _initialize_weights(self):
+        """Initialize model weights"""
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+                
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        
+        return logits
